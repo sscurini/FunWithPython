8c7815de0779feffc3134c9e0d345a09d379a839
Stephen Scurini <38360450+sscurini@users.noreply.github.com>
2019-11-09 11:08:51 -0500

    Add changes

---
+++ MainNameExample/ImportedProgram.py
@@ -0,0 +1,6 @@
+
+print("Imported program running")
+x = 5
+
+if __name__ == "__main__":
+    print("This only runs when its the main program FROM ImportedProgram")
---
+++ MainNameExample/MainProgram.py
@@ -0,0 +1,6 @@
+import ImportedProgram
+
+print("This is the maim program")
+
+if __name__ == "__main__":
+    print("This only runs when its the main program")
